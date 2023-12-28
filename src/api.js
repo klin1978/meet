b9ddee10b9cca6a32178f89968f25689b64e475c
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-12-28 06:47:14 -0600

    implement offline capability

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import mockData from './mock-data';
+import NProgress from 'nprogress';
 
 /*
  * @param {*} events:
@@ -26,6 +27,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events?JSON.parse(events):[];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -34,6 +41,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null; 
   }
